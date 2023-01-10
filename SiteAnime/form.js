6d9accab8c216eab3d99953e115b910a6cbc89ba
Carlos Gabriel <gabriel.s.modesto2@gmail.com>
2023-01-10 11:30:48 -0300

    Add files via upload

---
+++ SiteAnime/form.js
@@ -0,0 +1,19 @@
+function valida1() {
+    alert("Digite o nome completo!")
+}
+
+function valida2() {
+    alert("Digite seu melhor e-mail!")
+}
+
+function valida3() {
+    alert("Digite uma senha nível 5 e anote para não esquecer!! =)")
+}
+
+function valida4() {
+    alert("Maximo de 30 caracteres!")
+}
+
+function valida5() {
+    alert("Verifique se o formulário está preenchido corretamente!!")
+}
